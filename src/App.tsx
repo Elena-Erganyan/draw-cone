0af1355edb45a8896678ff386a6eacfdddacb221
Elena Erganyan <elena-erganyan@rambler.ru>
2023-10-04 16:56:15 +0300

    Working solution (frontend version)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,64 @@
+import { useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Cone from "./components/Cone";
 
 export default function App() {
+  const [radius, setRadius] = useState(1);
+  const [height, setHeight] = useState(2);
+  const [segmentsNumber, setSegmentsNumber] = useState(4);
+  const [vertices, setVertices] = useState<number[]>([]);
+
+  useEffect(() => {
+    setVertices(computeVertices(radius, height, segmentsNumber));
+  }, [radius, height, segmentsNumber]);
+
+  const computeVertices = (radius: number, height: number, segmentsNumber: number) => {
+    const coneVertex = [0, 0, height];
+    const vertices: number[] = [];
+    let currentVertex: number[];
+    let nextVertex: number[];
+    const firstVertex = [radius * Math.cos(2 * Math.PI * 0 / segmentsNumber), radius * Math.sin(2 * Math.PI * 0 / segmentsNumber), 0];
+
+    for (let i = 0; i < segmentsNumber; i++) {
+      currentVertex = i === 0 ? firstVertex : nextVertex!;
+      nextVertex = i === segmentsNumber - 1
+        ? firstVertex
+        : [radius * Math.cos(2 * Math.PI * (i + 1) / segmentsNumber), radius * Math.sin(2 * Math.PI * (i + 1) / segmentsNumber), 0];
+      vertices.push(...coneVertex, ...currentVertex, ...nextVertex);
+    }
+  
+    return vertices;
+  };
+
   return (
-    <Canvas style={{height: 500}}>
-      <ambientLight intensity={Math.PI / 2} />
-      <spotLight position={[5, 5, 5]} penumbra={1} decay={0} />
-      <pointLight position={[-10, -10, -10]} decay={0} />
-      <Cone />
-      <OrbitControls enableZoom={false} />
-    </Canvas>
-  )
+    <div style={{display: "flex", gap: "50px", padding: "10px"}}>
+      <form
+        style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", gap: "20px"}}
+      >
+        <label style={{display: "flex", flexDirection: "column", gap: "5px"}}>
+          Radius
+          <input type="number" value={radius} onChange={(evt) => setRadius(+evt.target.value)}/>
+        </label>
+        <label style={{display: "flex", flexDirection: "column", gap: "5px"}}>
+          Height
+          <input type="number" value={height} onChange={(evt) => setHeight(+evt.target.value)}/>
+          </label>
+        <label style={{display: "flex", flexDirection: "column", gap: "5px"}}>
+          Segments Number
+          <input type="number" value={segmentsNumber} onChange={(evt) => setSegmentsNumber(+evt.target.value)}/>
+        </label>
+      </form>
+
+      <Canvas style={{height: "calc(100vh - 40px)"}}>
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight position={[5, 5, 5]} decay={0} />
+        <pointLight position={[-10, -10, -10]} decay={0} />
+        <Cone segmentsNumber={segmentsNumber} vertices={vertices} />
+        <OrbitControls enableZoom={false} />
+      </Canvas>
+
+    </div>
+  );
 }
 
--- src/components/Cone.tsx
+++ src/components/Cone.tsx
@@ -1,43 +1,28 @@
-// import { useFrame } from "@react-three/fiber";
-import { useRef } from "react";
-import { DoubleSide, Mesh } from "three";
+import { DoubleSide } from "three";
 
-const Cone = (props: JSX.IntrinsicElements["mesh"]) => {
-  const ref = useRef<Mesh>(null!);
-  const radius = 1;
-  const height = 2;
-  const segmentsNumber = 8;
+interface ConeProps {
+  vertices: number[];
+  segmentsNumber: number;
+}
 
-  // useFrame(() => (ref.current.rotation.y += 0.005));
-
-  const computeTriangles = (radius: number, height: number, segmentsNumber: number) => {
-    const coneVertex = [0, 0, height];
-    const vertices: number[] = [];
-    let currentVertex: number[];
-    let nextVertex: number[];
-    const firstVertex = [radius * Math.cos(2 * Math.PI * 0 / segmentsNumber), radius * Math.sin(2 * Math.PI * 0 / segmentsNumber), 0];
-
-    for (let i = 0; i < segmentsNumber; i++) {
-      currentVertex = i === 0 ? firstVertex : nextVertex!;
-      nextVertex = i === segmentsNumber - 1
-        ? firstVertex
-        : [radius * Math.cos(2 * Math.PI * (i + 1) / segmentsNumber), radius * Math.sin(2 * Math.PI * (i + 1) / segmentsNumber), 0];
-      vertices.push(...coneVertex, ...currentVertex, ...nextVertex);
-    }
-  
-    return vertices;
-  };
-
-  const vertices = new Float32Array(computeTriangles(radius, height, segmentsNumber));
-
+const Cone = ({vertices, segmentsNumber}: ConeProps) =>  {
   return (
-    <mesh {...props} ref={ref} scale={1.5}>
-      <bufferGeometry attach="geometry">
-        <bufferAttribute attach="attributes-position" array={vertices} itemSize={3} count={segmentsNumber * 3} normalized />
+    <mesh scale={0.7} rotation={[5, 0.3, 0]}>
+      <bufferGeometry attach="geometry" key={JSON.stringify(vertices)}>
+        <bufferAttribute
+          attach="attributes-position"
+          array={new Float32Array(vertices)}
+          itemSize={3}
+          count={segmentsNumber * 3}
+        />
       </bufferGeometry>
-      <meshStandardMaterial attach="material" color="turquoise" side={DoubleSide} flatShading={true} />
+      <meshStandardMaterial
+        attach="material"
+        color="turquoise"
+        side={DoubleSide}
+        flatShading={true}
+      />
     </mesh>
-
   );
 };
 
